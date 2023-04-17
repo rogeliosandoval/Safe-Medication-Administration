c26acb06450d2e1d2076dfc7dad4be0e587e8dc1
Roger <rogelio.g.sandoval@gmail.com>
2023-04-16 21:34:15 -0500

    create safe-drug component

---
+++ src/app/safe-drug/safe-drug-component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'safe-drug',
+    templateUrl: './safe-drug.component.html',
+    styleUrls: ['./safe-drug.component.scss']
+})
+
+export class SafeDrugAdministrationComponent {
+
+}
