f98782863e8dd470f4553686f417c03f029de121
Roger <rogelio.g.sandoval@gmail.com>
2023-04-16 21:34:38 -0500

    create dosage component

---
+++ src/app/dosage/dosage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'dosage',
+    templateUrl: './dosage.component.html',
+    styleUrls: ['./dosage.component.scss']
+})
+
+export class SolvingDosageCalculationProblems {
+
+}
