e8fba4fd14dcdd72d9d9c34656eb8c1430a65c1d
Roger <rogelio.g.sandoval@gmail.com>
2023-04-16 21:34:29 -0500

    create principles component

---
+++ src/app/principles/principles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'principles',
+    templateUrl: 'principles.component.html',
+    styleUrls: ['./principles.component.scss']
+})
+
+export class PrinciplesOfAdministeringDrugs {
+
+}
