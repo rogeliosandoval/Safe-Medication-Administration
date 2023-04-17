195b1f3cb4b788d6a1a71bd433f9c7ee1348059b
Roger <rogelio.g.sandoval@gmail.com>
2023-04-16 21:33:39 -0500

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SafeDrugAdministrationComponent } from './safe-drug/safe-drug-component';
+import { PrinciplesOfAdministeringDrugs } from './principles/principles.component';
+import { SolvingDosageCalculationProblems } from './dosage/dosage.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'safe-drug-administration', component: SafeDrugAdministrationComponent },
+  { path: 'principles-of-administering-drugs', component: PrinciplesOfAdministeringDrugs },
+  { path: 'solving-dosage-calculation-problems', component: SolvingDosageCalculationProblems },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SafeDrugAdministrationComponent,
+    PrinciplesOfAdministeringDrugs,
+    SolvingDosageCalculationProblems
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
